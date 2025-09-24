11657ced773413a8631bf817d7ad187d163bfea3
Jamradise <denis_korablev2004@mail.ru>
2025-09-24 19:30:32 +0000

    added convert to tflite

--- configs/paths.py
+++ configs/paths.py
@@ -42,6 +42,7 @@ class PathConfig:
             PATH_TO_SAVE_DATASET = DETECTION_DATASET_DIR
             PATH_TO_SAVE_TRAIN_RUNS = YOLO_RUNS_DIR
             PATH_TO_YOLO_CONFIG = PATH_TO_YOLO_CONFIG_YAML
+            PATH_TO_BEST_YOLO_WEIGHTS = Path("/home/jamradise/MegaDendrologAI/ML/Detection/yolo_train/runs/train/20250923_195225/weights/best.pt")
 
         PATH_TO_RAW_IMAGES = DETECTION_DATASET_DIR / "Hack-raw-data"
         PATH_TO_SAVE_PROCESSED_IMAGES = DETECTION_DATASET_DIR / "Hack-processed-data"
