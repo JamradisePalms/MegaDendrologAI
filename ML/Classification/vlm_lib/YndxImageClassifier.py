a3f3e95990edce8ede24958e3268055976a09cd1
pasheee <pasha.panfilov.2000@mail.ru>
2025-09-30 15:31:15 +0300

    added yndx api classifier

---
+++ ML/Classification/vlm_lib/YndxImageClassifier.py
@@ -0,0 +1,52 @@
+from ML.Classification.vlm_lib.BaseClassifier import BaseClassifier
+from ML.Classification.vlm_lib.Parser import ResponseParser
+import requests
+import os
+from pydantic import BaseModel
+from pathlib import Path
+from typing import Type, Dict, Any
+SOY_TOKEN = os.environ.get("SOY_TOKEN")
+class YndxImageClassifier(BaseClassifier):
+    pydantic_model: BaseModel
+
+    def __init__(self, prompt_filepath, pydantic_model: Type[BaseModel]):
+        super().__init__(prompt_filepath)
+        self.pydantic_model = pydantic_model
+        self.parser = ResponseParser(pydantic_model)
+
+    def _single_request(self, image_path: Path) -> Dict[str:Any]:
+        url = "https://api.eliza.yandex.net/internal/vlm/yandex_vlm_pro_soy/generative"
+        model_name = "yandex_vlm_pro_soy"
+
+        messages = []
+
+        messages.append({"role": "user", "content": self.prompt})
+
+        content = []
+        content.append({"type": "text", "text": self.prompt})
+
+        base64_image, mime_type = self._encode_image(image_path)
+        content.append(
+            {
+                "type": "image_url",
+                "image_url": {"url": f"data:{mime_type};base64,{base64_image}"},
+            }
+        )
+
+        messages.append({"role": "user", "content": content})
+
+        payload = {
+            "model": model_name,
+            "messages": messages,
+        }
+        headers = {
+            "authorization": f"OAuth {SOY_TOKEN}",
+            "content-type": "application/json",
+        }
+
+        response = requests.post(url, json=payload, headers=headers, verify=False)
+        response.raise_for_status()
+        result_json = response.json()["response"]["choices"][0]["message"]["content"]
+        parsed_answer = self.parser.parse(result_json)
+
+        return self.pydantic_model.model_validate(parsed_answer)
